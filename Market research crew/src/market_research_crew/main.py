62b273e3e590c66c17a180556507694622469636
Anant Bishnoi <218342032+AnantBishnoi-29@users.noreply.github.com>
2026-01-21 16:04:31 +0530

    market research crew

---
+++ Market research crew/src/market_research_crew/main.py
@@ -0,0 +1,21 @@
+
+import warnings
+from market_research_crew.crew import MarketResearchCrew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        "product_idea" : "An AI powered tool that summraizes youtube video son my channel and post the summary on various social media platforms like linkedin, intsagram, facebook , X , whatsapp "
+    }
+
+    try:
+        MarketResearchCrew().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
